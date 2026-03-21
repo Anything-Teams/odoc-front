fe277df97f35bca583691412c9b629ede05c6f6c
anything <hcd1228@gmail.com>
2026-03-22 02:40:21 +0900

    설정변경

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -12,8 +12,8 @@ export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigate to="/login" replace />} />
-    
+                <Route index element={<Navigate to="/login" replace />} />
+
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
 
