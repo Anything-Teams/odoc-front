e3a92fb47117140a850c7c97ab8cb11172b137b2
anything <hcd1228@gmail.com>
2026-03-21 19:52:32 +0900

    odoc frontend init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/test")
+      .then(res => res.text())
+      .then(setData);
+  }, []);
+
+  return <div>{data}</div>;
 }
 
 export default App;
