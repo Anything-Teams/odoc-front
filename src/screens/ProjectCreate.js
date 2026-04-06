e1657d72e3f60a4b8490cab56def0be28b64db87
anything <hcd1228@gmail.com>
2026-04-07 01:04:33 +0900

    디자인 수정

--- src/screens/ProjectCreate.js
+++ src/screens/ProjectCreate.js
@@ -89,103 +89,111 @@ export default function ProjectCreate() {
   }
 
   return (
-    <div className="create-container">
-      <div></div>
-      <div className="create-inner">
-        <h1 className="logo">{odocType === "1" ? "ODOC 명" : "기록명"}</h1>
-
-        <input type="text" placeholder={`${odocType === "1" ? "목표 습관명(10자제한)" : "기록명(10자제한)"}`} className="input" value={odocNm} onChange={(e) => setOdocNm(e.target.value)} maxLength={10}/>
+    <div className="project-container">
+      <div className="project-detail">
         
-        <div className="odoc-type-setting">
-          <label className={`radio-item odoc-item ${odocType === "1" ? "odoc-active" : ""}`}>
-            <input
-              type="radio" name="odocType" value="1"
-              checked={odocType === "1"}
-              onChange={(e) => updateOdocType(e.target.value)}
-            />
-            <span className="odoc-item-span">ODOC</span>
-          </label>
-
-          <label className={`radio-item odoc-item ${odocType === "2" ? "odoc-active" : ""}`}>
-            <input
-              type="radio" name="odocType" value="2"
-              checked={odocType === "2"}
-              onChange={(e) => updateOdocType(e.target.value)}
-            />
-            <span className="odoc-item-span">기록</span>
-          </label>
+        <div className="detail-title">
+          
+          <div className="input-wrapper">
+            <div className="odocNm-class">{odocType === "1" ? "ODOC 명" : "기록명"}</div>
+          </div>  
         </div>
 
-        <div className="project-create-image">
+
+        <div className="create-inner">
+
+          <input type="text" placeholder={`${odocType === "1" ? "목표 습관명(10자제한)" : "기록명(10자제한)"}`} className="input" value={odocNm} onChange={(e) => setOdocNm(e.target.value)} maxLength={10}/>
+          
           <div className="odoc-type-setting">
-            <label className={`radio-item odoc-item ${onAlarm === "Y" ? "odoc-active" : ""}`}>
+            <label className={`radio-item odoc-item ${odocType === "1" ? "odoc-active" : ""}`}>
               <input
-                type="radio" name="onAlarm" value="Y"
-                checked={onAlarm === "Y"}
-                onChange={(e) => updateOnAlarm(e.target.value)}
+                type="radio" name="odocType" value="1"
+                checked={odocType === "1"}
+                onChange={(e) => updateOdocType(e.target.value)}
               />
-              <span className="odoc-item-span">알람 ON</span>
+              <span className="odoc-item-span">ODOC</span>
             </label>
 
-            <label className={`radio-item odoc-item ${onAlarm === "N" ? "odoc-active" : ""}`}>
+            <label className={`radio-item odoc-item ${odocType === "2" ? "odoc-active" : ""}`}>
               <input
-                type="radio" name="onAlarm" value="N"
-                checked={onAlarm === "N"}
-                onChange={(e) => updateOnAlarm(e.target.value)}
+                type="radio" name="odocType" value="2"
+                checked={odocType === "2"}
+                onChange={(e) => updateOdocType(e.target.value)}
               />
-              <span className="odoc-item-span">알람 OFF</span>
+              <span className="odoc-item-span">기록</span>
             </label>
-            <input
-              type="time"
-              className="alarm-time"
-              value={odocAlarmTime}
-              onChange={(e) => setOdocAlarmTime(e.target.value)}
-            />
           </div>
 
-          <div className="image-box create">
-              <img
-                    src={isInsertOdocThema?`/images/empty/0.png`:`/images/${odocThemaType}/0.png`}
-                    alt="progress"
-                    className="image-placeholder create"
+          <div className="project-create-image">
+            <div className="odoc-type-setting">
+              <label className={`radio-item odoc-item ${onAlarm === "Y" ? "odoc-active" : ""}`}>
+                <input
+                  type="radio" name="onAlarm" value="Y"
+                  checked={onAlarm === "Y"}
+                  onChange={(e) => updateOnAlarm(e.target.value)}
+                />
+                <span className="odoc-item-span">알람 ON</span>
+              </label>
+
+              <label className={`radio-item odoc-item ${onAlarm === "N" ? "odoc-active" : ""}`}>
+                <input
+                  type="radio" name="onAlarm" value="N"
+                  checked={onAlarm === "N"}
+                  onChange={(e) => updateOnAlarm(e.target.value)}
+                />
+                <span className="odoc-item-span">알람 OFF</span>
+              </label>
+              <input
+                type="time"
+                className="alarm-time"
+                value={odocAlarmTime}
+                onChange={(e) => setOdocAlarmTime(e.target.value)}
               />
-          </div>
-          <span className="thema-desc">{themaDesc}</span>
-
-          {isInsertOdocThema && (
-            <div className="odoc-main-stream">
-              {alertMessage}
             </div>
-          )}
 
-          <div className="odoc-type-setting">
-            <div className="thema-list">
-              {odocThemaList.map((item) => (
-                <label key={item.themaId} className={`radio-item thema-item ${odocThemaType === item.themaId? "thema-active" : ""}`}>
-                  <input
-                    type="radio" name="odocThemaType" value={item.themaId}
-                    checked={odocThemaType === item.themaId}
-                    onChange={() => setThema(item)}
-                  />
-                  <span className="thema-item-span">{item.themaNm}</span>
-                </label>
-              ))}
+            <div className="image-box create">
+                <img
+                      src={isInsertOdocThema?`/images/empty/0.png`:`/images/${odocThemaType}/0.png`}
+                      alt="progress"
+                      className="image-placeholder create"
+                />
+            </div>
+            <span className="thema-desc">{themaDesc}</span>
+
+            {isInsertOdocThema && (
+              <div className="odoc-main-stream">
+                {alertMessage}
+              </div>
+            )}
+
+            <div className="odoc-type-setting">
+              <div className="thema-list">
+                {odocThemaList.map((item) => (
+                  <label key={item.themaId} className={`radio-item thema-item ${odocThemaType === item.themaId? "thema-active" : ""}`}>
+                    <input
+                      type="radio" name="odocThemaType" value={item.themaId}
+                      checked={odocThemaType === item.themaId}
+                      onChange={() => setThema(item)}
+                    />
+                    <span className="thema-item-span">{item.themaNm}</span>
+                  </label>
+                ))}
+              </div>
             </div>
           </div>
+
+          <button className="btn primary" disabled={isInsertOdocThema}
+            onClick={() => insertProject()}
+          >
+            추가
+          </button>
+          <button className="btn secondary"
+            onClick={() => navigate(-1)}
+          >
+            취소
+          </button>
         </div>
-
-        <button className="btn primary" disabled={isInsertOdocThema}
-          onClick={() => insertProject()}
-        >
-          추가
-        </button>
-        <button className="btn secondary"
-          onClick={() => navigate(-1)}
-        >
-          취소
-        </button>
       </div>
-      <div></div>
     </div>
   );
 }
